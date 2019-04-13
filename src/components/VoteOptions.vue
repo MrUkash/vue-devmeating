2d3c923e3b40da6a45616661a61abcbdcc81bc04
Lukasz Kuska <lukasz.kuska@pirios.com>
2019-04-13 14:21:40 +0200

    [#32] Nie dziala lol

---
+++ src/components/VoteOptions.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="width: 300px;">
+    <ul>
+      <slot v-for="optionItem in options" name="item" :option-item="optionItem">
+        <li :key="option.id">{{optionItem.name}}</li>
+      </slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VoteOptions",
+  data() {
+    return {
+      //   options: ["tak", "nie"],
+      options: [
+        {
+          id: 0,
+          name: "tak"
+        },
+        {
+          id: 1,
+          name: "nie"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    <VoteOptions>
+      <template v-slot:item="{optionItem}">
+        <li>
+          <strong>{{optionItem.name}}</strong>
+        </li>
+      </template>
+    </VoteOptions>
   </div>
 </template>
 
 <script>
+import VoteOptions from "../components/VoteOptions";
 export default {
-  name: "home"
+  name: "home",
+  components: { VoteOptions },
+  data() {
+    return {
+      options: ["tak", "może", "nie wiem", "nie"]
+    };
+  }
 };
 </script>
