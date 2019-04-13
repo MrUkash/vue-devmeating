3080408a4a38945d7a3a8c52a156ff198f814123
Lukasz Kuska <lukasz.kuska@pirios.com>
2019-04-13 10:53:15 +0200

    [#14] Guard

--- src/router.js
+++ src/router.js
@@ -1,23 +1,40 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Vote from "./views/Vote.vue";
+import Score from "./views/Score.vue";
+import Settings from "./views/Settings.vue";
 
-Vue.use(Router)
+Vue.use(Router);
+
+const adminGuard = (to, from, next) => {
+  const isAdmin = localStorage.getItem("isAdmin");
+  if (!isAdmin) next({ path: "/home" });
+  next();
+};
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: "/vote",
+      name: "vote",
+      beforeEnter: adminGuard,
+      component: Vote
+    },
+    {
+      path: "/score",
+      name: "score",
+      component: Score
+    },
+    {
+      path: "/settings",
+      name: "settings",
+      component: Settings
     }
   ]
-})
+});
