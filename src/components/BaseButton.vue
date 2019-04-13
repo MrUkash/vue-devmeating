698e44aec0ebeb53217b64b63f4c3c2552e94ec3
Lukasz Kuska <lukasz.kuska@pirios.com>
2019-04-13 11:33:54 +0200

    [#21] Przycisk z isLoading

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button type="button" v-on="$listeners">{{`Click Me! (${loading})`}}</button>
+</template>
+
+<script>
+export default {
+  props: { loading: Boolean }
+};
+</script>
+
+<style>
+</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,13 +1,23 @@
 <template>
+  <div>
     <h1>Settings</h1>
+    <h2>{{`isLoading: ${isLoading}`}}</h2>
+
+    <BaseButton :loading="isLoading" @click="isLoading=!isLoading"></BaseButton>
+  </div>
 </template>
 
 <script>
+import BaseButton from "../components/BaseButton";
 export default {
-
-}
+  components: { BaseButton },
+  data() {
+    return {
+      isLoading: false
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
