83691c69a005824189e724501f515034f08feec4
Lukasz Kuska <lukasz.kuska@pirios.com>
2019-04-13 11:47:03 +0200

    [#22] BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label for="a">{{label}}</label>
+    <input id="a" type="text" :value="newValue" @input="$emit('input', $event.target.value)">
+  </div>
+</template>
+
+<script>
+export default {
+  props: { label: String, value: { type: String, required: true } },
+  data() {
+    return {
+      newValue: this.value
+    };
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Score.vue
+++ src/views/Score.vue
@@ -1,13 +1,24 @@
 <template>
+  <div>
     <h1>Score</h1>
+
+    <h2>{{input}}</h2>
+    <BaseInput v-model="input" label="BaseInput "></BaseInput>
+  </div>
 </template>
 
 <script>
-export default {
+import BaseInput from "../components/BaseInput";
 
-}
+export default {
+  components: { BaseInput },
+  data() {
+    return {
+      input: ""
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
