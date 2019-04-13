469a6a294b51800fb0c1bced0c57fc59094ee134
Lukasz Kuska <lukasz.kuska@pirios.com>
2019-04-13 10:53:02 +0200

    [#13] Nawigacja

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/vote">Vote</router-link>|
+      <router-link to="/score">Score</router-link>|
+      <router-link to="/settings">Settings</router-link>
     </div>
     <router-view/>
   </div>
@@ -10,7 +12,7 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
